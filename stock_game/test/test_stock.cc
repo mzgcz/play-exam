6cfe1ebb4e8fdf195a847af39d6db8d2f520805e
mzgcz <mzgcz@self-sys>
2017-02-17 21:11:47 +0800

    set include path in cmake

--- stock_game/test/test_stock.cc
+++ stock_game/test/test_stock.cc
@@ -1,5 +1,5 @@
 #include <gtest/gtest.h>
-#include "../stock.hh"
+#include "stock.hh"
 
 using namespace std;
 
--- stock_game/test/test_trader.cc
+++ stock_game/test/test_trader.cc
@@ -1,5 +1,5 @@
 #include <gtest/gtest.h>
-#include "../trader.hh"
+#include "trader.hh"
 
 using namespace std;
 
